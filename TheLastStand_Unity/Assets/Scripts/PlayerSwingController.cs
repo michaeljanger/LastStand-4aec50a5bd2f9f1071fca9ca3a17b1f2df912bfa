755a8b1fc4ed0a487ba453b0b4658a3a10627381
michaeljanger <michaeljanger@gmail.com>
2017-12-07 23:38:00 -0800

    added swing and hit feature

--- TheLastStand_Unity/Assets/Scripts/PlayerSwingController.cs
+++ TheLastStand_Unity/Assets/Scripts/PlayerSwingController.cs
@@ -14,6 +14,8 @@ public class PlayerSwingController : MonoBehaviour {
 	public bool PlayerSwingisLeft;
 	public bool PlayerSwingisRight;
 	public bool PlayerSwingisIdle;
+	public bool PlayerHorAttack;
+	public bool PlayerVerAttack;
 
 	// Use this for initialization
 	void Start () {
@@ -24,77 +26,126 @@ public class PlayerSwingController : MonoBehaviour {
 		PlayerSwingisRight = false;
 		PlayerSwingisLeft = false;
 		PlayerSwingisIdle = true;
+		PlayerHorAttack = false;
+		PlayerVerAttack = false;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float t = Input.GetAxis ("RightTrigger");
 		float x = Input.GetAxis("4th axis");
 		float y = Input.GetAxis("5th axis");
+		//Debug.Log (t); //debug right trigger
+		if (t < -0.80 && PlayerSwingisIdle == false) { //initiate swing
+			Debug.Log("P1 Swing");
 
-		if (x == 0 && y == 0 && PlayerSwingisIdle == false) {
+
+			characteranim.Play ("");
+
+			if (PlayerSwingisUp == true) {
+				characteranim.Play ("upattack");
+
+				PlayerSwingisUp = false;
+				PlayerSwingisIdle = true;
+
+
+			} 
+				if (PlayerSwingisRight == true) {
+					characteranim.Play ("rightattack");
+				PlayerSwingisRight = false;
+				PlayerSwingisIdle = true;
+				PlayerHorAttack = true;
+
+				} 
+					if (PlayerSwingisLeft == true) {
+						characteranim.Play ("leftattack");
+				PlayerSwingisLeft = false;
+				PlayerSwingisIdle = true;
+				PlayerHorAttack = true;
+					} 
+						if (PlayerSwingisDown == true) {
+							characteranim.Play ("downattack");
+				PlayerSwingisDown = false;
+				PlayerSwingisIdle = true;
+						} 
+			}
+
+		Debug.Log ("PlayerHorAttack" + PlayerHorAttack);
+		//Debug.Log ("PlayerSwingIdle" + PlayerSwingisIdle);
+		//Debug.Log ("PlayerSwingUp" + PlayerSwingisUp);
+		//Debug.Log ("PlayerSwingDown" + PlayerSwingisDown);
+		//Debug.Log ("PlayerSwingLeft" + PlayerSwingisLeft);
+		//Debug.Log ("PlayerSwingRight" + PlayerSwingisRight);
+
+		if (x == 0 && y == 0 && PlayerSwingisIdle == true) {
 
 			//play anim
 			PlayerSwingisIdle = true;
+			PlayerSwingisUp = false;
+			PlayerSwingisDown = false;
+			PlayerSwingisRight = false;
+			PlayerSwingisLeft = false;
+
+	
 			if (PlayerSwingisIdle == true) {
 				characteranim.Play ("idle");
-				Debug.Log ("idle");
+				PlayerHorAttack = false;
+				//Debug.Log ("idle");
 			}
 		}
 		else 
 		{
-			PlayerSwingisIdle = false;
+			//PlayerSwingisIdle = false;
 		}
 		//right swing
-		if (x == 1 && PlayerSwingisIdle == false  && PlayerSwingisRight == false) {
+		if (x == 1 && PlayerSwingisIdle == true  && PlayerSwingisRight == false) {
 			PlayerSwingisRight = true;
 			PlayerSwingisIdle = false;
+			PlayerHorAttack = false;
 			//play anim
 			characteranim.Play ("rightswing");
 			Debug.Log ("rightswing");
 		}
-		else 
-		{
-			PlayerSwingisRight = false;
-		}
+
 		//left swing
-		if (x == -1  && PlayerSwingisIdle == false && PlayerSwingisLeft == false) {
+		if (x == -1  && PlayerSwingisIdle == true && PlayerSwingisLeft == false) {
 			PlayerSwingisLeft = true;
 			PlayerSwingisIdle = false;
+			PlayerHorAttack = false;
 			//play anim
 			Debug.Log("leftswing");
 			characteranim.Play ("leftswing");
 		}
-		else 
-		{
-			PlayerSwingisLeft = false;
-		}
+
 		//up swing
-		if (y == -1 && PlayerSwingisIdle == false && PlayerSwingisUp == false) {
+		if (y == -1 && PlayerSwingisIdle == true && PlayerSwingisUp == false) {
 			PlayerSwingisUp = true;
 			PlayerSwingisIdle = false;
 			//play anim
 			characteranim.Play ("upswing");
 			Debug.Log("upswing");
-		}
-		else 
-		{
-			PlayerSwingisUp = false;
+			PlayerVerAttack = true;
 		}
 
+
 		//down swing
-		if (y == 1 && PlayerSwingisIdle == false && PlayerSwingisDown == false) {
+		if (y == 1 && PlayerSwingisIdle == true && PlayerSwingisDown == false) {
 			PlayerSwingisDown = true;
 
 			//play anim
 			characteranim.Play ("downswing");
 			Debug.Log("downswing");
+			PlayerVerAttack = true;
 		}
-		else 
-		{
-			PlayerSwingisDown = false;
-		}
+
+
+	}
+	public void PlayerAttack ()
+	{
+		PlayerHorAttack = false;
+		PlayerVerAttack = false;
+		PlayerSwingisIdle = true;
 
 	}
 }
--- TheLastStand_Unity/Assets/Scripts/killed.cs
+++ TheLastStand_Unity/Assets/Scripts/killed.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class killed : MonoBehaviour {
 	public GameObject object1;
 	public GameObject object2;
+	public bool playerhorattacking;
 	public bool enemyalive;
 	// Use this for initialization
 	void Start () {
+		
 		enemyalive = true;
 	}
 	
@@ -16,9 +18,11 @@ public class killed : MonoBehaviour {
 		
 	}
 	void OnTriggerEnter(Collider other){
+		playerhorattacking = GameObject.Find("Player_idle").GetComponent<PlayerSwingController>().PlayerHorAttack;
 		Debug.Log ("col enemy");
-		if (enemyalive == true && (other.gameObject.CompareTag("PlayerTag") || other.gameObject.CompareTag("Player2Tag")))
+		if (enemyalive == true && playerhorattacking == true && (other.gameObject.CompareTag("PlayerTag") || other.gameObject.CompareTag("Player2Tag")))
 		{
+			
 			Debug.Log ("killed enemy");
 			Instantiate(object1, transform.position, transform.rotation);
 			Instantiate(object2, transform.position, transform.rotation);
